5382c3c1f1114e9b64c70ee650552802d480cb63
AADARSH KUMAR <95774710+aadarshkumarr@users.noreply.github.com>
2023-01-28 04:26:59 +0530

    complete
    ready

---
+++ js/commands.js
@@ -0,0 +1,89 @@
+var linkedin = 'https://www.linkedin.com/in/aadarshkumarr/';
+var github = "https://github.com/aadarshkumarr";
+var email = 'mailto:aadarshkm01@gmail.com';
+var resume = "https://drive.google.com/file/d/1cOS_4pgIouR3QupzznQR_mF6hyVf8Paw/view?usp=sharing";
+var exit = "https://aadarshkumar.in/";
+
+
+whois = [
+  "<br>",
+  "Hey, I'm Aadarsh Kumar!👋",
+  "I'm a software developer and Full Stack Developer, who builds engaging websites like this one",
+  "After graduating with a Bachelor's of Computer Application, I'm doing my Master's of Computer Application",
+  "as a software engineer building enterprise web applications is my work.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="https://www.linkedin.com/in/aadarshkumarr/">linkedin/aadarshkumar' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/aadarshkumar' + "</a>",
+  "<br>"
+];
+
+resume = [
+  "<br>",
+  'resume       <a href="' + resume + '"https://drive.google.com/file/d/1cOS_4pgIouR3QupzznQR_mF6hyVf8Paw/view?usp=sharing">resume/aadarshkumar' + "</a>",
+  "<br>"
+];
+
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  'Opening github         <a href="' + github + '" target="_blank">github/aadarshkumar' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Aadarsh Kumar?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Send an email to Aadarsh Kumar',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">resume</span>         Display the Resume',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">exit</span>           Exit the terminal',
+  "<br>",
+];
+
+exit = [
+  "<br>",
+  'exit       <a href="' + exit + '" target="https://aadarshkumar.in/">exit/aadarshkumar' + "</a>",
+  "<br>"
+];
+
+
+banner = [
+  '<span class="index">AADARSH KUMAR (AK) Not A Corporation. All rights reserved.</span>',
+
+  "________  _______  ______   _______  _______  _______             _                 _______  _______  _______ ",
+  "(  ___  )(  ___  )(  __  \\ (  ___  )(  ____ )(  ____ \\|\\     /|  | \\    /\\|\\     /|(       )(  ___  )(  ____ )",
+  "| (   ) || (   ) || (  \\  )| (   ) || (    )|| (    \\/| )   ( |  |  \\  / /| )   ( || () () || (   ) || (    )|",
+  "| (___) || (___) || |   ) || (___) || (____)|| (_____ | (___) |  |  (_/ / | |   | || || || || (___) || (____)|",
+  "|  ___  ||  ___  || |   | ||  ___  ||     __)(_____  )|  ___  |  |   _ (  | |   | || |(_)| ||  ___  ||     __)",
+  "| (   ) || (   ) || |   ) || (   ) || (\\ (         ) || (   ) |  |  ( \\ \\ | |   | || |   | || (   ) || (\\ (   ",
+  "| )   ( || )   ( || (__/  )| )   ( || ) \\ \\__/\\____) || )   ( |  |  /  \\ \\| (___) || )   ( || )   ( || ) \\ \\__",
+  "|/     \\||/     \\|(______/ |/     \\||/   \\__/\\_______)|/     \\|  |_/    \\/(_______)|/     \\||/     \\||/   \\__/",
+  "                                                                                                        © 2023",
+  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",  
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
